a31bf5fc7ab42743ca75e07935964493fd988f36
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2024-06-27 19:36:17 +0700

    KDN-PC-02: Update

---
+++ src/Helpers/Helpers.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Container\Container;
+
+if (!function_exists('app')) {
+	function app($abstract = null, array $parameters = []) {
+		if (is_null($abstract)) {
+			return Container::getInstance();
+		}
+
+		return Container::getInstance()->make($abstract, $parameters);
+	}
+}
+else {
+	function wpspcore_app($abstract = null, array $parameters = []) {
+		if (is_null($abstract)) {
+			return Container::getInstance();
+		}
+
+		return Container::getInstance()->make($abstract, $parameters);
+	}
+}
