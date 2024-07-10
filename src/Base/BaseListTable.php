97bd703f0764bc72ea4df383e39efc574f1de7b6
Khanh Duy Nguyen <khanhpkvn@gmail.com>
2024-07-10 13:13:29 +0700

    KDN-PC-01: Update

--- src/Base/BaseListTable.php
+++ src/Base/BaseListTable.php
@@ -4,8 +4,8 @@ namespace WPSPCORE\Base;
 
 abstract class BaseListTable extends \WP_List_Table {
 
-	public ?string $defaultOrder   = null;
-	public ?string $defaultOrderby = null;
+	public ?string $defaultOrder   = 'asc';
+	public ?string $defaultOrderby = 'id';
 	public ?array  $removeQueryVars = [];
 
 	public function __construct($args = []) {
