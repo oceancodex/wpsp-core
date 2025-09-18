b3aaac08dad01c22c69df870cffefed46ed92964
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-09-18 12:03:33 +0700

    KDN-PC-01: Update

--- src/Base/BaseShortcode.php
+++ src/Base/BaseShortcode.php
@@ -4,10 +4,50 @@ namespace WPSPCORE\Base;
 
 abstract class BaseShortcode extends BaseInstances {
 
-	public function init($atts, $content, $tag) {
-		return $this->index($atts, $content, $tag);
+	public mixed $shortcode         = null;
+	public mixed $callback_function = null;
+	public mixed $custom_properties = null;
+
+	/*
+	 *
+	 */
+
+	public function __construct($mainPath = null, $rootNamespace = null, $prefixEnv = null, $shortcode = null, $callback_function = null, $custom_properties = null) {
+		parent::__construct($mainPath, $rootNamespace, $prefixEnv);
+		$this->callback_function = $callback_function;
+		$this->custom_properties = $custom_properties;
+		$this->overrideShortcode($shortcode);
+		$this->customProperties();
 	}
 
-	abstract public function index($atts, $content, $tag);
+	/*
+	 *
+	 */
+
+	public function init($shortcode = null): void {
+		$callback = $this->callback_function ? [$this, $this->callback_function] : null;
+		$shortcode = $this->shortcode ?? $shortcode;
+		if ($shortcode) {
+			add_shortcode($shortcode, $callback);
+		}
+	}
+
+	/*
+	 *
+	 */
+
+	protected function overrideShortcode($shortcode = null): void {
+		if ($shortcode && !$this->shortcode) {
+			$this->shortcode = $shortcode;
+		}
+	}
+
+	/*
+	 *
+	 */
+
+//	abstract public function index($atts, $content, $tag);
+
+	abstract public function customProperties();
 
 }
