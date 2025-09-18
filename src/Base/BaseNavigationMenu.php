e3271c22cad262d3956416ecf4f45a0d31278d75
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-09-18 11:29:09 +0700

    KDN-PC-01: Update

--- src/Base/BaseNavigationMenu.php
+++ src/Base/BaseNavigationMenu.php
@@ -9,7 +9,7 @@ abstract class BaseNavigationMenu extends BaseInstances {
 
 	use ObjectPropertiesToArrayTrait;
 
-	public              $args     = null;
+	public mixed        $args     = null;
 	public static ?self $instance = null;
 
 	/*
@@ -27,7 +27,11 @@ abstract class BaseNavigationMenu extends BaseInstances {
 
 	public static function render() {
 		self::instance()->args->echo = false;
-		return wp_nav_menu(self::instance()->args->toArray());
+		$args = self::instance()->args->toArray();
+		if (wp_get_nav_menu_object($args['menu'])) {
+			return wp_nav_menu($args);
+		}
+		return false;
 	}
 
 	protected static function instance(): ?self {
