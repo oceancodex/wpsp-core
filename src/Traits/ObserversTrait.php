b96d66f3a692aa294e3aaa7af3f9524d8a0270c7
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-10-15 18:25:41 +0700

    KDN-PC-01: Update

--- src/Traits/ObserversTrait.php
+++ src/Traits/ObserversTrait.php
@@ -9,7 +9,7 @@ trait ObserversTrait {
 	protected static function boot() {
 		parent::boot();
 		if (!empty(self::$observers)) {
-			static::setEventDispatcher(new \Illuminate\Events\Dispatcher());
+//			static::setEventDispatcher(new \Illuminate\Events\Dispatcher());
 			foreach (self::$observers as $observer) {
 				static::observe(new $observer());
 			}
