3cc2b21a3f85f031e30e4d8d668321710bcd00f9
khanh <khanhpkvn@gmail.com>
2025-11-01 18:56:27 +0700

    KDN-PC-02: Update

--- src/Traits/AdminPagesRouteTrait.php
+++ src/Traits/AdminPagesRouteTrait.php
@@ -48,7 +48,7 @@ trait AdminPagesRouteTrait {
 		if (!empty($callback) && is_admin() && !wp_doing_ajax() && !wp_doing_cron() && !$this->funcs->_wantsJson()) {
 			$requestPath = trim($this->request->getRequestUri(), '/\\');
 			if (
-				is_callable($callback) || is_null($callback[1]) && (
+				is_array($callback) || is_callable($callback) || is_null($callback[1]) && (
 					!isset($callback[1]) || $callback[1] == 'index'
 					|| $this->request->get('page') == $fullPath
 					|| preg_match('/' . $this->funcs->_escapeRegex($fullPath) . '$/iu', $requestPath)
@@ -58,7 +58,7 @@ trait AdminPagesRouteTrait {
 					$constructParams = [
 						[
 							'path'              => $fullPath,
-							'callback_function' => $callback[1] ?? null,
+							'callback_function' => $callback instanceof \Closure ? $callback : $callback[1] ?? null,
 							'validation'        => $this->validation,
 							'custom_properties' => $customProperties,
 						],
