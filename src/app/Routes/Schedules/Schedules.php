f201ed2d49fa630c6426b30e6eedf56b2b938172
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-12-19 14:05:18 +0700

    KDN-PC-01: Update

--- src/app/Routes/Schedules/Schedules.php
+++ src/app/Routes/Schedules/Schedules.php
@@ -16,37 +16,28 @@ class Schedules extends BaseRoute {
 	 * RouteManager::executeAllRoutes()
 	 */
 	public function execute($route) {
-		$hook        = $route->fullPath;
+		$path        = $route->path;
+		$fullPath    = $route->fullPath;
 		$callback    = $route->callback;
-		$interval    = $route->args['interval'] ?? 'daily';
+		$interval    = $route->args['interval'] ?? 'hourly';
 		$requestPath = trim($this->request->getRequestUri(), '/\\');
-
-		// Đăng ký schedule nếu chưa tồn lại.
-		if (!wp_next_scheduled($hook)) {
-			wp_schedule_event(time(), $interval, $hook);
-		}
-
-		// Xóa schedule khi plugin bị hủy kích hoạt
-		register_deactivation_hook($this->funcs->_getMainFilePath(), function() use ($hook) {
-			wp_unschedule_hook($hook);
-//			$timestamp = wp_next_scheduled($hook);
-//			if ($timestamp) wp_unschedule_event($timestamp, $hook);
-		});
 
 		$constructParams = [
 			$this->funcs->_getMainPath(),
 			$this->funcs->_getRootNamespace(),
 			$this->funcs->_getPrefixEnv(),
 			[
-				'hook'              => $hook,
+				'path'              => $path,
+				'full_path'         => $fullPath,
+				'interval'          => $interval,
 				'callback_function' => $callback[1] ?? null,
 			],
 		];
 
-		// Chạy callback của Schedule khi được gọi.
+		// Init schedule.
 		$callback[1] = 'init';
-		$callback = $this->prepareRouteCallback($callback, $constructParams);
-		$callParams = $this->getCallParams($hook, $hook, $requestPath, $callback[0], $callback[1]);
+		$callback    = $this->prepareRouteCallback($callback, $constructParams);
+		$callParams  = $this->getCallParams($path, $fullPath, $requestPath, $callback[0], $callback[1]);
 		$this->resolveAndCall($callback, $callParams);
 	}
 
--- src/app/WordPress/Schedules/BaseSchedule.php
+++ src/app/WordPress/Schedules/BaseSchedule.php
@@ -10,7 +10,7 @@ abstract class BaseSchedule extends BaseInstances {
 	use RouteTrait;
 
 	public $hook              = null;
-	public $interval          = 'hourly';
+	public $interval          = null;
 	public $callback_function = null;
 
 	/*
@@ -19,12 +19,19 @@ abstract class BaseSchedule extends BaseInstances {
 
 	public function afterConstruct() {
 		$this->callback_function = $this->extraParams['callback_function'] ?? null;
-		$this->overrideHook($this->extraParams['hook'] ?? null);
+		$this->overrideInterval($this->extraParams['interval'] ?? null);
+		$this->overrideHook($this->extraParams['full_path'] ?? null);
 	}
 
-	private function overrideHook($hook = null) {
+	public function overrideHook($hook = null) {
 		if ($hook && !$this->hook) {
 			$this->hook = $hook;
+		}
+	}
+
+	public function overrideInterval($interval = null) {
+		if ($interval && !$this->interval) {
+			$this->interval = $interval;
 		}
 	}
 
@@ -32,26 +39,56 @@ abstract class BaseSchedule extends BaseInstances {
 	 *
 	 */
 
-	public function init($hook = null, $interval = 'hourly') {
-		$callback = null;
-		$callbackFunction = $this->callback_function;
-		if ($callbackFunction && method_exists(static::class, $callbackFunction)) {
+	public function init($hook = null, $interval = null) {
+		$hook     = $hook ?? $this->hook;
+		$interval = $interval ?? $this->interval;
+
+		// Đăng ký schedule nếu chưa tồn lại.
+		if (!wp_next_scheduled($hook)) {
+			wp_schedule_event(time(), $interval, $hook);
+		}
+
+		// Đăng ký action gắn với schedule.
+		add_action($hook, [$this, $this->callback_function . '!']);
+
+		// Xóa schedule khi plugin bị hủy kích hoạt
+		register_deactivation_hook($this->funcs->_getMainFilePath(), function() use ($hook) {
+			wp_unschedule_hook($hook);
+//			$timestamp = wp_next_scheduled($hook);
+//			if ($timestamp) wp_unschedule_event($timestamp, $hook);
+		});
+	}
+
+	/*
+	 *
+	 */
+
+	public function __call($method, $arguments) {
+		/**
+		 * Xử lý DI cho callback function tại đây.\
+		 * Trong route Schedules khai báo schedule với callback function: "handle"\
+		 * Callback function thực tế được add vào WordPress sẽ là: "handle!"\
+		 * Sử dụng __call() để bắt call "handle!" và xử lý DI.
+		 */
+		$method = preg_replace('/!$/', '', $method);
+		if (method_exists($this, $method)) {
 			$requestPath = trim($this->request->getRequestUri(), '/\\');
+
 			$constructParams = [
 				$this->funcs->_getMainPath(),
 				$this->funcs->_getRootNamespace(),
 				$this->funcs->_getPrefixEnv(),
 				[
-					'path'              => $hook ?? $this->hook,
-					'full_path'         => $hook ?? $this->hook,
-					'callback_function' => $callbackFunction,
+					'hook'              => $hook ?? $this->hook,
+					'interval'          => $interval ?? $this->interval,
+					'callback_function' => $this->callback_function,
 				],
 			];
-			$callback = $this->prepareRouteCallback([static::class, $callbackFunction], $constructParams);
-			$callParams = $this->getCallParams($this->hook, $this->hook, $requestPath, static::class, $callbackFunction);
-			$callback = $this->resolveCallback($callback, $callParams);
+
+			$callback   = $this->prepareRouteCallback([$this, $this->callback_function], $constructParams);
+			$callParams = $this->getCallParams($this->hook, $this->hook, $requestPath, $this, $this->callback_function);
+			$this->resolveAndCall($callback, $callParams);
 		}
-		add_action($hook, $callback);
 	}
 
 }
