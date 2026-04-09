78a323bfc96083033b64440fe77db15fd627c7e3
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-04-09 19:31:56 +0700

    KDN-PC-02: Update

--- src/app/Console/Commands/Extends/WipeCommand.php
+++ src/app/Console/Commands/Extends/WipeCommand.php
@@ -2,6 +2,8 @@
 
 namespace WPSPCORE\App\Console\Commands\Extends;
 
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use WPSPCORE\App\Console\Traits\CommandsTrait;
 
 class WipeCommand extends \Illuminate\Database\Console\WipeCommand {
@@ -18,7 +20,7 @@ class WipeCommand extends \Illuminate\Database\Console\WipeCommand {
 	 * This is mainly useful when a lot of commands extends one main command
 	 * where some things need to be initialized based on the input arguments and options.
 	 */
-	protected function initialize($input, $output) {
+	protected function initialize(InputInterface $input, OutputInterface $output) {
 		parent::initialize($input, $output);
 		$this->funcs = $this->laravel->make('funcs');
 	}
