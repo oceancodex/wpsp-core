24cf4c7b2d8f953964f409f346bce6d15f737e95
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-04-09 19:20:18 +0700

    KDN-PC-02: Update

--- src/app/Console/Commands/Extends/KeyGenerateCommand.php
+++ src/app/Console/Commands/Extends/KeyGenerateCommand.php
@@ -3,6 +3,8 @@
 namespace WPSPCORE\App\Console\Commands\Extends;
 
 use Illuminate\Foundation\Console\KeyGenerateCommand as Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use WPSPCORE\App\Console\Traits\CommandsTrait;
 
 class KeyGenerateCommand extends Command {
@@ -32,7 +34,7 @@ class KeyGenerateCommand extends Command {
 	 * This is mainly useful when a lot of commands extends one main command
 	 * where some things need to be initialized based on the input arguments and options.
 	 */
-	protected function initialize($input, $output) {
+	protected function initialize(InputInterface $input, OutputInterface $output): void {
 		parent::initialize($input, $output);
 		$this->funcs = $this->laravel->make('funcs');
 	}
