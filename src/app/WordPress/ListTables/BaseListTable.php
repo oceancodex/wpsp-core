dab6f588b30fee33f0cbd0d250d26c92e8e692bc
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-04-25 20:01:24 +0700

    KDN-PC-01: Update .env

--- src/app/WordPress/ListTables/BaseListTable.php
+++ src/app/WordPress/ListTables/BaseListTable.php
@@ -8,14 +8,23 @@ abstract class BaseListTable extends \WP_List_Table {
 
 	use BaseInstancesTrait;
 
-	public $removeQueryVars = [];
+	public $screenOptionsKey = null;
 
-	public function __construct($args = [], $mainPath = null, $rootNamespace = null, $prefixEnv = null, $extraParams = []) {
+	/*
+	 *
+	 */
+
+	public function __construct($args = [], $screenId = null, $mainPath = null, $rootNamespace = null, $prefixEnv = null, $extraParams = []) {
 		// Cần gọi __construct của parent trước.
 		parent::__construct($args);
 
 		// Khởi tạo các thuộc tính cơ bản.
 		$this->baseInstanceConstruct($mainPath, $rootNamespace, $prefixEnv, $extraParams);
+
+		// Chuẩn hóa "screenOptionsKey".
+		if (!$this->screenOptionsKey) {
+			$this->screenOptionsKey = $this->funcs->_slugParams(['page']) ?? $screenId;
+		}
 
 		// Tự động đăng ký các checkboxes để ẩn/hiện cột cho Custom List Table trên Screen Options panel.
 		$this->autoScreenOptionColumns();
@@ -29,6 +38,14 @@ abstract class BaseListTable extends \WP_List_Table {
 			$screenId = $screen->id;
 			$showScreenOptions = $screen->show_screen_options ?? false;
 			if ($showScreenOptions) {
+				// Nếu screen ID hiện tại không khớp với screenOptionsKey của list table, không khởi tạo sreen options panel.
+				if (is_array($this->screenOptionsKey)) {
+					if (!in_array($screenId, $this->screenOptionsKey)) return;
+				}
+				elseif (is_string($this->screenOptionsKey)) {
+					if ($screenId !== $this->screenOptionsKey) return;
+				}
+
 				add_filter("manage_{$screenId}_columns", function($columns) {
 					$columns = array_merge($columns, $this->get_columns());
 					return $columns;
