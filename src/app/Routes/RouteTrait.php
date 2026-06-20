915abe307f55fd74b0d942f1142d128e8ec7e864
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-06-20 07:18:02 +0700

    KDN-PC-02: Update

--- src/app/Routes/RouteTrait.php
+++ src/app/Routes/RouteTrait.php
@@ -677,8 +677,11 @@ trait RouteTrait {
 		// Set container và facade theo mỗi lần gọi callback.
 		Container::setInstance($container);
 		Facade::setFacadeApplication($container);
-		Model::setConnectionResolver($container['db']);
-		Model::setEventDispatcher($container['events']);
+
+		if (class_exists('Illuminate\Foundation\Application')) {
+			Model::setConnectionResolver($container['db']);
+			Model::setEventDispatcher($container['events']);
+		}
 
 		if (!$call) {
 			return function(...$wpParams) use ($container, $callback, $callParams) {
