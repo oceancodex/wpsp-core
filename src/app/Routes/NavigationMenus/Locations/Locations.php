9f697493733db84927280e6419ed64e096fd41fa
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-07-07 10:59:11 +0700

    KDN-PC-02: Update

--- src/app/Routes/NavigationMenus/Locations/Locations.php
+++ src/app/Routes/NavigationMenus/Locations/Locations.php
@@ -18,9 +18,11 @@ class Locations extends BaseRoute {
 	public function execute($route) {
 		$requestPath = ltrim($this->request->getRequestUri(), '/\\');
 
-		$path     = $route->path;
-		$fullPath = $route->fullPath;
-		$callback = $route->callback;
+		$path          = $route->path;
+		$pathRegex     = $route->pathRegex;
+		$fullPath      = $route->fullPath;
+		$fullPathRegex = $route->fullPathRegex;
+		$callback      = $route->callback;
 
 		$constructParams = [
 			$this->funcs->_getMainPath(),
@@ -28,7 +30,9 @@ class Locations extends BaseRoute {
 			$this->funcs->_getPrefixEnv(),
 			[
 				'path'              => $path,
+				'path_regex'        => $pathRegex,
 				'full_path'         => $fullPath,
+				'full_path_regex'   => $fullPathRegex,
 				'callback_function' => $callback[1] ?? null,
 			],
 		];
--- src/app/Routes/Taxonomies/Taxonomies.php
+++ src/app/Routes/Taxonomies/Taxonomies.php
@@ -18,10 +18,13 @@ class Taxonomies extends BaseRoute {
 	public function execute($route) {
 		$requestPath = ltrim($this->request->getRequestUri(), '/\\');
 
-		$path        = $route->path;
-		$fullPath    = $route->fullPath;
-		$callback    = $route->callback;
-		$middlewares = $route->middlewares;
+		$path          = $route->path;
+		$pathRegex     = $route->pathRegex;
+		$fullPath      = $route->fullPath;
+		$fullPathRegex = $route->fullPathRegex;
+		$callback      = $route->callback;
+		$middlewares   = $route->middlewares;
+
 		if ($this->isPassedMiddleware($middlewares, $this->request, ['route' => $route])) {
 			$constructParams = [
 				$this->funcs->_getMainPath(),
@@ -29,7 +32,9 @@ class Taxonomies extends BaseRoute {
 				$this->funcs->_getPrefixEnv(),
 				[
 					'path'              => $path,
+					'path_regex'        => $pathRegex,
 					'full_path'         => $fullPath,
+					'full_path_regex'   => $fullPathRegex,
 					'callback_function' => $callback[1] ?? null,
 				],
 			];
