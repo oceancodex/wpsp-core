bc7e197e5d918b3f0cf83e1e3355de6c66a9ae5c
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-07-15 17:18:46 +0700

    KDN-PC-02: Update

--- src/app/WordPress/AdminPages/BaseAdminPage.php
+++ src/app/WordPress/AdminPages/BaseAdminPage.php
@@ -17,7 +17,7 @@ abstract class BaseAdminPage extends BaseInstances {
 	public 	$menu_slug              = null;
 	public 	$icon_url               = null;
 	public 	$position               = null;
-	public 	$parent_slug            = '@';
+	public 	$parent_slug            = null;
 
 	public 	$classes                = null;
 	public 	$firstSubmenuTitle      = null;
